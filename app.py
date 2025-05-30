7bc1655dac5663d3116df7c702b443697509ed7b
Paxai <101057177+Paxai@users.noreply.github.com>
2025-05-30 21:35:34 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request, render_template
+import json
+
+app = Flask(__name__)
+
+OFFSET_FILE = 'offsets.json'
+
+@app.route('/api/offsets')
+def get_offsets():
+    with open(OFFSET_FILE) as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit_offsets():
+    if request.method == 'POST':
+        data = dict(request.form)
+        with open(OFFSET_FILE, 'w') as f:
+            json.dump(data, f, indent=2)
+    with open(OFFSET_FILE) as f:
+        offsets = json.load(f)
+    return render_template('editor.html', offsets=offsets)
+
+if __name__ == '__main__':
+    app.run()
